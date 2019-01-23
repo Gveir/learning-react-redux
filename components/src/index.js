c3255b0f47f6a9bd795acd388ae1e0400088d2ff
Gveir <kri5s.prog@gmail.com>
2019-01-23 22:23:54 +0100

    Approval card with custom content

--- components/src/index.js
+++ components/src/index.js
@@ -11,26 +11,31 @@ const App = () => {
   ];
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Sam"
-        avatarSrc={faker.image.avatar()}
-        timeAgo="Today at 6:00PM"
-        comment={comments[0]}
-        text="Nice blog post!"
-      />
-      <CommentDetail
-        author="Luke"
-        avatarSrc={faker.image.avatar()}
-        timeAgo="Today at 6:00AM"
-        text="Very nice blog post!"
-      />
-      <CommentDetail
-        author="Jack"
-        avatarSrc={faker.image.avatar()}
-        timeAgo="Yesterday at 2:00PM"
-        text="Interesting blog post!"
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          avatarSrc={faker.image.avatar()}
+          timeAgo="Today at 6:00PM"
+          comment={comments[0]}
+          text="Nice blog post!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Luke"
+          avatarSrc={faker.image.avatar()}
+          timeAgo="Today at 6:00AM"
+          text="Very nice blog post!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jack"
+          avatarSrc={faker.image.avatar()}
+          timeAgo="Yesterday at 2:00PM"
+          text="Interesting blog post!"
+        />
+      </ApprovalCard>
     </div>
   );
 };
