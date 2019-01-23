9043d09e0f80a99674dfcb953f1e1e86b08066d0
Gveir <kri5s.prog@gmail.com>
2019-01-23 21:23:44 +0100

    Added CommentDetail component

--- components/src/index.js
+++ components/src/index.js
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 
+import CommentDetail from "./CommentDetail";
+
 const App = () => {
   return (
     <div className="ui container comments">
+      <CommentDetail />
       <div className="comment">
         <a href="/" className="avatar">
           <img alt="avatar" src={faker.image.avatar()} />
