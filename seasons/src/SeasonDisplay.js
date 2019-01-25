1a1dbfdb4f25fc63017c4eba97bb3d992bb3e9f1
Gveir <kri5s.prog@gmail.com>
2019-01-25 12:02:56 +0100

    Seasons app - reafctoring

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,23 +1,31 @@
 import React from "react";
 
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: "sun"
+  },
+  winter: {
+    text: "Burr, it's chilly!",
+    iconName: "snowflake"
+  }
+};
 const getSeason = (latitude, month) => {
   if (month > 2 && month < 9) {
     return latitude > 0 ? "summer" : "winter";
   } else {
-    return latitude < 0 ? "summer" : "winter";
+    return latitude > 0 ? "winter" : "summer";
   }
 };
 
 const SeasonDisplay = props => {
   const season = getSeason(props.latitude, new Date().getMonth());
-  const icon = season === "winter" ? "snowflake" : "sun";
+  const { text, iconName } = seasonConfig[season];
   return (
     <div>
-      <i className={`icon ${icon}`} />
-      <h1>
-        {season === "summer" ? "Let's hit the beach!" : "Burr, it's chilly!"}
-      </h1>
-      <i className={`icon ${icon}`} />
+      <i className={`icon ${iconName}`} />
+      <h1>{text}</h1>
+      <i className={`icon ${iconName}`} />
     </div>
   );
 };
