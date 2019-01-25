2bf731608421690334c8a0260e6efea35e38acdd
Gveir <kri5s.prog@gmail.com>
2019-01-25 11:57:16 +0100

    Seasons app - showing icons

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -10,11 +10,14 @@ const getSeason = (latitude, month) => {
 
 const SeasonDisplay = props => {
   const season = getSeason(props.latitude, new Date().getMonth());
+  const icon = season === "winter" ? "snowflake" : "sun";
   return (
     <div>
+      <i className={`icon ${icon}`} />
       <h1>
         {season === "summer" ? "Let's hit the beach!" : "Burr, it's chilly!"}
       </h1>
+      <i className={`icon ${icon}`} />
     </div>
   );
 };
