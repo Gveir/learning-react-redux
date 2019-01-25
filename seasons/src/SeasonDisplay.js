58a5932218edfed378f6e031ccc25063ead37036
Gveir <kri5s.prog@gmail.com>
2019-01-25 12:18:03 +0100

    Seasons app - styling

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./SeasonDisplay.css";
 
 const seasonConfig = {
   summer: {
@@ -22,10 +23,10 @@ const SeasonDisplay = props => {
   const season = getSeason(props.latitude, new Date().getMonth());
   const { text, iconName } = seasonConfig[season];
   return (
-    <div>
-      <i className={`icon ${iconName}`} />
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive icon ${iconName}`} />
       <h1>{text}</h1>
-      <i className={`icon ${iconName}`} />
+      <i className={`icon-right massive icon ${iconName}`} />
     </div>
   );
 };
