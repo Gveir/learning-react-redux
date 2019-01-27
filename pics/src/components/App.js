93961ce0bba7d86c9c41481d49e3bf61f65e5944
Gveir <kri5s.prog@gmail.com>
2019-01-27 13:47:17 +0100

    Pics - calling unsplash.com API

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import axios from "axios";
+
+// const unsplashCredentials = {
+//   clientId: "Access Key from unsplash.com dev app here"
+// };
+import unsplashCredentials from "../unsplashCredentials";
 
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      headers: {
+        Authorization: `Client-ID ${unsplashCredentials.clientId}`
+      },
+      params: {
+        query: term,
+        per_page: 20
+      }
+    });
   }
 
   render() {
