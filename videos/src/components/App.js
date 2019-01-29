221cd86881b8d206d431ad78468c0700a23f1ebd
Gveir <kri5s.prog@gmail.com>
2019-01-29 22:35:14 +0100

    Videos - selecting video on the list

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -5,7 +5,7 @@ import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async term => {
     const response = await youtube.get("/search", { params: { q: term } });
@@ -13,11 +13,20 @@ class App extends React.Component {
     this.setState({ videos: response.data.items });
   };
 
+  onVideoSelect = video => {
+    this.setState({ selectedVideo: video });
+
+    console.log(video);
+  };
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={this.onVideoSelect}
+        />
       </div>
     );
   }
--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -2,9 +2,15 @@ import React from "react";
 
 import VideoItem from "./VideoItem";
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
   const videoItems = videos.map(video => {
-    return <VideoItem key={video.id} video={video} />;
+    return (
+      <VideoItem
+        key={video.id.videoId}
+        video={video}
+        onVideoSelect={onVideoSelect}
+      />
+    );
   });
 
   return <div className="ui relaxed divided list">{videoItems}</div>;
