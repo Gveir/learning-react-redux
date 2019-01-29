ce35ee61dc093a7928ff6c73e0273d8aa1615bfd
Gveir <kri5s.prog@gmail.com>
2019-01-29 23:05:38 +0100

    Videos - final touches

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -8,10 +8,17 @@ import VideoDetail from "./VideoDetail";
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
 
+  componentDidMount() {
+    this.onTermSubmit("falafel na pełnej");
+  }
+
   onTermSubmit = async term => {
     const response = await youtube.get("/search", { params: { q: term } });
 
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
   };
 
   onVideoSelect = video => {
@@ -22,11 +29,19 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          videos={this.state.videos}
-          onVideoSelect={this.onVideoSelect}
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- videos/src/components/VideoDetail.js
+++ videos/src/components/VideoDetail.js
@@ -8,7 +8,10 @@ const VideoDetail = ({ video }) => {
   return (
     <div>
       <div className="ui embed">
-        <iframe src={`https://www.youtube.com/embed/${video.id.videoId}`} />
+        <iframe
+          src={`https://www.youtube.com/embed/${video.id.videoId}`}
+          title="video player"
+        />
       </div>
       <div className="ui segment">
         <h4 className="ui header">{video.snippet.title}</h4>
