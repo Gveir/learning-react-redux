6154860c48354c261b895a7f5455afe5e4ee7e5d
Gveir <kri5s.prog@gmail.com>
2019-02-03 22:39:13 +0100

    Blog - (not so good) solution to fetching a user multiple times

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 import jsonPlaceholder from "../api/jsonPlaceholder";
 
 export const fetchPosts = () => async dispatch => {
@@ -6,8 +8,10 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = userId => async dispatch => {
+export const fetchUser = userId => dispatch => _fetchUser(userId, dispatch);
+
+const _fetchUser = _.memoize(async (userId, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${userId}`);
 
   dispatch({ type: "FETCH_USER", payload: response.data });
-};
+});
