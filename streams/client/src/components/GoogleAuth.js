b6e81cb3bc4a01a0b4dd22cad809fc844e537b95
Gveir <kri5s.prog@gmail.com>
2019-02-05 22:22:07 +0100

    Streams - added initial google authentication

---
+++ streams/client/src/components/GoogleAuth.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+//clientId: "your client id"
+import clientId from "../googleClientId";
+
+class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId: clientId,
+          scope: "email"
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
+    });
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know if we are signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
+  }
+
+  render() {
+    return <div>{this.renderAuthButton()}</div>;
+  }
+}
+
+export default GoogleAuth;
