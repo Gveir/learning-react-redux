312f34cdeedeb67d0a4141eaab0570bf9b37ce62
Gveir <kri5s.prog@gmail.com>
2019-02-15 23:01:52 +0100

    Translate - using context in place of redux

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,26 +1,20 @@
 import React from "react";
 
-import LanguageContext from "../context/LanguageContext";
+import { LanguageStore } from "../context/LanguageContext";
 import ColorContext from "../context/ColorContext";
 import UserCreate from "./UserCreate";
 import LanguageSelector from "./LanguageSelector";
 
 class App extends React.Component {
-  state = { language: "english" };
-
-  onLanguageChange = language => {
-    this.setState({ language });
-  };
-
   render() {
     return (
       <div className="ui container">
-        <LanguageSelector onLanguageChange={this.onLanguageChange} />
-        <ColorContext.Provider value="red">
-          <LanguageContext.Provider value={this.state.language}>
+        <LanguageStore>
+          <LanguageSelector />
+          <ColorContext.Provider value="red">
             <UserCreate />
-          </LanguageContext.Provider>
-        </ColorContext.Provider>
+          </ColorContext.Provider>
+        </LanguageStore>
       </div>
     );
   }
